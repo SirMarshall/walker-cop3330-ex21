5fa7d5ad794d62066acc9f99ab7450ef4b8e337e
SirMarshall <carterwalker@knights.ucf.edu>
2021-09-13 23:16:22 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,66 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Carter I. Walker
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Necessary Variables
+        Scanner input = new Scanner(System.in);
+        int number;
+        String monthName = "";
+
+
+        //Number query
+        System.out.println("Please enter the number of the month:");
+        number = input.nextInt();
+
+        switch (number)
+        {
+            case 1:
+                monthName = "January";
+                break;
+            case 2:
+                monthName = "February";
+                break;
+            case 3:
+                monthName = "March";
+                break;
+            case 4:
+                monthName = "April";
+                break;
+            case 5:
+                monthName = "May";
+                break;
+            case 6:
+                monthName = "June";
+                break;
+            case 7:
+                monthName = "July";
+                break;
+            case 8:
+                monthName = "August";
+                break;
+            case 9:
+                monthName = "September";
+                break;
+            case 10:
+                monthName = "October";
+                break;
+            case 11:
+                monthName = "November";
+                break;
+            case 12:
+                monthName = "December";
+                break;
+            default:
+                System.out.println("Invalid number.");
+        }
+        System.out.println("The name of the month is " + monthName);
+    }
+}
